4ee8ad0273c701e27552653a208017a9576c3467
Patrick McKnight <pem725@gmail.com>
2019-10-07 10:07:14 -0400

    added ggplotRegression and lmerICCest to the monolithic function file.  Will break up later by function and document.

--- PEMfunctions.R
+++ PEMfunctions.R
@@ -17,7 +17,27 @@ UniPlot <- function(x,binwidth=.5){
   
   p1 + geom_text(x=xcent,y=ymax,label=paste("MEAN = ",round(xbar,2))) +
     geom_text(x=xcent,y=(ymax-.1*(ymax)),label=paste("SD = ",round(std,2)))
-  
-  
+}
+
+ggplotRegression <- function(fit){
+  require(ggplot2)
+  require(ggExtra)
+  p <- ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+    geom_point() +
+    stat_smooth(method = "lm", col = "red") +
+    geom_smooth() +
+    labs(title = paste("Corr = ",signif(sqrt(summary(fit)$r.squared),2),
+                       "Adj R2 = ",signif(summary(fit)$adj.r.squared, 3),
+                       "Intercept =",signif(fit$coef[[1]],3 ),
+                       " Slope =",signif(fit$coef[[2]], 3),
+                       " P =",signif(summary(fit)$coef[2,4], 3)))
+  ggExtra::ggMarginal(p, type="histogram", xparams = list(col="blue", fill="orange"), yparams = list(col="orange", fill="blue"))
+}
+
+
+lmerICCest <- function(x,facet=NULL){
+  tmp <- as.data.frame(VarCorr(x))[,c("grp","vcov")]
+  out <- round(tmp$vcov[!is.na(match(tmp$grp,facet))]/sum(tmp$vcov),2)
+  return(out)
 }
 
